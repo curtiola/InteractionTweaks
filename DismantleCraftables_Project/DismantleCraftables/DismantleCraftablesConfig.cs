5abfb09143492e5ecf585313814aad7eaffe5a77
Curtiola <curtiola.courier175@slmail.me>
2025-03-29 23:53:52 -0600

    Update .NET, SMAPI versions & trim the fat

--- DismantleCraftables_Project/DismantleCraftables/DismantleCraftablesConfig.cs
+++
@@ -1,5 +0,0 @@
-﻿public class DismantleCraftablesConfig
-{
-    public StardewModdingAPI.SButton dismantle_key { get; set; } = StardewModdingAPI.SButton.R;
-    public int loss { get; set; } = 49;
-}
